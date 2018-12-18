483d3a8484107eccd2449dd2d3f34a37fdcba406
Nguyen Duc Hoang <sunlight4d@gmail.com>
2018-12-18 13:22:12 +0700

    update

--- 03-Build a responsive Login form using Bootstrap 4, HTML 5, CSS 3/code/BootstrapExample/jquery/index.js
+++ 03-Build a responsive Login form using Bootstrap 4, HTML 5, CSS 3/code/BootstrapExample/jquery/index.js
@@ -1,30 +1,34 @@
+/*
+Nguyen Duc Hoang (Mr)
+Youtube channel: https://www.youtube.com/c/nguyenduchoang
+Programming tutorial channel
+*/
 function isEmail(inputEmail) {
-	var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
 	return regex.test(inputEmail);
 }
 function validatePassword(inputPassword) {
-	return inputPassword.length() > 4;
+	return inputPassword.length > 4;
 }
 
-$(document).ready(function (){
-	$("#email").change(function(){				
-		var email = this.val().strip();
-		if(!isEmail(email)) {
-			$("#email").html("Email is not valid format");
+$(document).ready(function(){
+    $('#email').change(function(){
+        var email = $(this).val().trim();
+        // alert(`email = ${JSON.stringify(email)}`)
+        if(!isEmail(email)) {
+            //Error ?
+            $(".emailError").html("Email is not valid format");
+        } else {
+            $(".emailError").html("");
+        }
+    });
+    $('#password').change(function(){
+        var password = $(this).val();	
+        if(!validatePassword(password)) {
+			$(".passwordError").html("Password must be at least 5 characters");
 		} else {
-			$("#email").html("");
+			$(".passwordError").html("");
 		}
-	});
-	$("#password").change(function(){				
-		var password = this.val();		
-		if(!isEmail(password)) {
-			$("#password").html("Password must be at least 5 characters");
-		} else {
-			$("#password").html("");
-		}
-	});
+    });
 });
-
-
-
 
