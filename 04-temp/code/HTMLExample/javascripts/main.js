3f4d9de815490f80d6fbdc94f3e1106d11e32f00
Nguyen Duc Hoang <sunlight4d@gmail.com>
2018-12-24 23:59:25 +0700

    update

--- 04-temp/code/HTMLExample/javascripts/main.js
+++ 04-temp/code/HTMLExample/javascripts/main.js
@@ -1,8 +1,22 @@
 var employees = []
+var startId = 0
+function loadInputEmployee() {
+    var name = document.getElementById('name').value
+    var dateOfBirth = new Date(document.getElementById('dob').value)
+    var gender = document.getElementById('gender').value
+    var languages = document.querySelector('.checkboxLanguage:checked').value
+    var department = document.querySelector('#department :selected').value    
+    return {name, dateOfBirth, gender, languages, department}
+}
 function btnAddDepartment() {
-    alert('ssss')
+    
 }
 function btnAddEmployee() {
+    var newDepartment = loadInputEmployee()
+    newDepartment = startId + 1
+    employees.push(newDepartment)
+}
+function fetchDataToTable() {
 
 }
 function btnDeleteEmployee() {
